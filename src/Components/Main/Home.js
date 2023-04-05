66145e30fc42ceb3cd86d62b797cba1cd3492ed9
Grégoire Paulet <gregoirepaulet84@gmail.com>
2023-04-05 16:50:00 +0200

    réalisation des ancres de la barre de nav

--- src/Components/Main/Home.js
+++ src/Components/Main/Home.js
@@ -14,12 +14,40 @@ const Home = () => {
     const [isLoaded, setIsLoaded] = useState(false);
     const [isEntered, setIsEntered] = useState(false);
     const [scrollY, setScrollY] = useState(0);
-    const headerRef = useRef(null)
 
-    // useEffect(() => {
-    //     console.log('=== LOADING ===');
-    //     console.log(isLoaded);
-    // }, [isLoaded])
+    const headerRef = useRef(null);
+    const companyRef = useRef(null);
+    const aboutRef = useRef(null);
+    const servicesRef = useRef(null);
+    const carsRef = useRef(null);
+    const bookingRef = useRef(null);
+
+    function navigate(section) {
+        switch(section) {
+            case 'company':
+                companyRef.current.scrollIntoView(({behavior: "smooth"}));
+                break;
+
+            case 'about':
+                aboutRef.current.scrollIntoView(({behavior: "smooth"}));
+                break;
+
+            case 'services':
+                servicesRef.current.scrollIntoView(({behavior: "smooth"}));
+                break;
+
+            case 'cars':
+                carsRef.current.scrollIntoView(({behavior: "smooth"}));
+                break;
+
+            case 'booking':
+                bookingRef.current.scrollIntoView(({behavior: "smooth"}));
+                break;
+
+            default:
+                break;
+        }
+    }
 
     useEffect(() => {
         // console.log(scrollY);
@@ -61,19 +89,19 @@ const Home = () => {
                 <nav>
                     <ul>
                         
-                        <li>
+                        <li onClick={() => navigate('about')}>
                             <div className="shine"></div>
                             À propos
                         </li>
-                        <li>
+                        <li onClick={() => navigate('services')}>
                             <div className="shine"></div>
                             Services
                         </li>
-                        <li>
+                        <li onClick={() => navigate('cars')}>
                             <div className="shine"></div>
                             Véhicules
                         </li>
-                        <li>
+                        <li onClick={() => navigate('booking')}>
                             <div className="shine"></div>
                             Réservations
                         </li>
@@ -85,21 +113,27 @@ const Home = () => {
                 </div>
             </header>
             <main>  
-                <section className="company-info-ctr">
+                <section className="company-info-ctr" ref={companyRef} >
                     <Company setIsLoaded={setIsLoaded} isEntered={isEntered} />
+                    <div className="top-link" onClick={() => navigate('company')}>
+                        <svg width="30" height="47" viewBox="0 0 40 47" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M20.0001 0L0 46.2434L20.0001 36.1916L40 46.2434L20.0001 0Z" fill="white"/>
+                        </svg>
+                        <p>Haut de page</p>
+                    </div>
                 </section>
                 <div className="transition"></div>
-                <section className="about-ctr">
+                <section className="about-ctr" ref={aboutRef}>
                     <About scrollY={scrollY} />
                 </section>
                 <div className="transition inverted"></div>
-                <section className="services-ctr">
+                <section className="services-ctr" ref={servicesRef}>
                     <Services scrollY={scrollY} />  
                 </section>
-                <section className='cars-ctr'>
+                <section className='cars-ctr' ref={carsRef}>
                     <Cars scrollY={scrollY} />
                 </section>
-                <section className="booking-ctr">
+                <section className="booking-ctr" ref={bookingRef}>
                     <Booking />
                 </section>
             </main>
